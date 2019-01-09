4de8e5c02811dfcd78d67b9933df2bdfc5a97bce
Stephen <stephen@nowhere>
2019-01-09 16:53:36 -0500

    Use stored --force-unpack to support models without zip extension

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -xe
 
 pip install --upgrade pip stored
-stored sync $SERVING_MODEL /model/1
+stored sync --force-unpack $SERVING_MODEL /model/1
 
 cd /opt
 foreman start
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model-serving',
-    version='0.0.5',
+    version='1.0.0',
     description='A tensorflow-serving model server via HTTP.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
