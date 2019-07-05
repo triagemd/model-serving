d39160d2f01efff58b61afe4445fe6f3d66a3988
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2019-07-05 14:08:42 -0400

    Bump version, remove pytest-flask from requeriments.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model-serving',
-    version='1.0.0',
+    version='1.0.1',
     description='A tensorflow-serving model server via HTTP.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
